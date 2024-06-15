ac482fe8e3175541d36a3fff143bcc231783f60f
szymonpolitowski <szymon.politowski.96@gmail.com>
2024-06-15 18:16:41 +0200

    web server: add wifi ssid and password config

--- src/wifi_web.cpp
+++ src/wifi_web.cpp
@@ -17,7 +17,7 @@ static void initWifiAccessPoint(void);
 static void handleRoot();
 static void handleSubmit();
 static void handleShutdown();
-// static void handleButton();
+static String maskPassword(String password);
 
 void initWifiWebServer(void)
 {
@@ -50,10 +50,12 @@ static void initWifiAccessPoint(void)
     Serial.println(WiFi.softAPIP());
 }
 
-static void handleRoot() {
-
+static void handleRoot()
+{
     String nameStr = String(getDeviceName().c_str());
     String intervalStr = String(std::to_string(getDeviceInterval()).c_str());
+    String ssidStr = String(getWiFiSSID().c_str());
+    String passStr = String(getWiFiPassword().c_str());
 
     String html = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'>";
     html += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
@@ -64,24 +66,36 @@ static void handleRoot() {
     html += "<input type=\"text\" id=\"field_dev_name\" name=\"field_dev_name\" value=\"" + nameStr + "\"><br><br>";
     html += "<label for=\"field_dev_interval\">Interval seconds: </label>";
     html += "<input type=\"text\" id=\"field_dev_interval\" name=\"field_dev_interval\" value=\"" + intervalStr + "\"><br><br>";
+    html += "<label for=\"field_wifi_ssid\">WiFi SSID: </label>";
+    html += "<input type=\"text\" id=\"field_wifi_ssid\" name=\"field_wifi_ssid\" value=\"" + ssidStr + "\"><br><br>";
+    html += "<label for=\"field_wifi_pass\">WiFi Password: </label>";
+    html += "<input type=\"password\" id=\"field_wifi_pass\" name=\"field_wifi_pass\" value=\"" + passStr + "\"><br><br>";
     html += "<input type=\"submit\" value=\"Submit\">";
     html += "</form><br>";
     html += "<button onclick=\"location.href='/shutdown'\">Shutdown Web Server</button>";
     html += "</body></html>";
 
     server.send(200, "text/html", html);
+
 }
 
-static void handleSubmit() {
-
+static void handleSubmit()
+{
     String temp_device_name;
     String temp_device_interval;
+    String temp_wifi_ssid;
+    String temp_wifi_pass;
 
     if (server.method() == HTTP_POST) {
         temp_device_name = server.arg("field_dev_name");
         temp_device_interval = server.arg("field_dev_interval");
+        temp_wifi_ssid = server.arg("field_wifi_ssid");
+        temp_wifi_pass = server.arg("field_wifi_pass");
+
         Serial.println("WebServer Device name: " + temp_device_name);
         Serial.println("WebServer Device interval: " + temp_device_interval);
+        Serial.println("WebServer WiFi SSID: " + temp_wifi_ssid);
+        Serial.println("WebServer WiFi Password: " + temp_wifi_pass);
 
         String response = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'>";
         response += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
@@ -89,18 +103,21 @@ static void handleSubmit() {
         response += "<h1>Data has been stored in internal memory</h1>";
         response += "<p>Device name: " + temp_device_name + "</p>";
         response += "<p>Interval seconds: " + temp_device_interval + "</p>";
+        response += "<p>WiFi SSID: " + temp_wifi_ssid + "</p>";
+        response += "<p>WiFi Password: " + maskPassword(temp_wifi_pass) + "</p>";
         response += "<button onclick=\"location.href='/'\">Go to Main Page</button>";
         response += "<button onclick=\"location.href='/shutdown'\">Shutdown Web Server</button>";
         response += "</body></html>";
 
         server.send(200, "text/html", response);
+
+        setDeviceName(std::string(temp_device_name.c_str()));
+        setDeviceInterval(std::stoul(std::string(temp_device_interval.c_str())));
+        setWiFiSSID(std::string(temp_wifi_ssid.c_str()));
+        setWiFiPassword(std::string(temp_wifi_pass.c_str()));
     } else {
         server.send(405, "text/html", "<h1>405 Method Not Allowed</h1>");
     }
-
-    setDeviceName(std::string(temp_device_name.c_str()));
-    setDeviceInterval(std::stoul(std::string(temp_device_interval.c_str())));
-
 }
 
 static void handleShutdown() {
@@ -110,4 +127,16 @@ static void handleShutdown() {
     WiFi.softAPdisconnect(true);
     Serial.println("Server stopped and AP disconnected");
     web_status = false;
+}
+
+static String maskPassword(String password)
+{
+    unsigned int len = password.length();
+    String masked_password;
+    masked_password.clear();
+    for(unsigned int i = 0; i < len; i++)
+    {
+        masked_password += '*';
+    }
+    return masked_password;
 }
