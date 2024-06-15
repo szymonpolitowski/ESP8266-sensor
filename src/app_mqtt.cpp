c9f27549e78fa6d35f5f05b0353fe14771a3fda7
szymonpolitowski <szymon.politowski.96@gmail.com>
2024-06-15 20:27:05 +0200

    mqtt: add commented code to publish message

--- src/app_mqtt.cpp
+++ src/app_mqtt.cpp
@@ -7,6 +7,9 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+// static void sendTestMessage(void);
+// static unsigned long msg_timestamp;
+
 void callback(char* topic, byte* message, unsigned int length) {
     Serial.print("Message arrived [");
     Serial.print(topic);
@@ -46,13 +49,33 @@ void mqttInit(void)
     addr_ip_mqtt_server.fromString(getMqttServer().c_str());
     client.setServer(addr_ip_mqtt_server, (uint16_t)std::stoul(getMqttPort().c_str()));
     client.setCallback(callback);
+    
+    // msg_timestamp = millis();
 }
 
 void mqttLoop(void)
 {
+    // if(5000 < millis() - msg_timestamp)
+    // {
+    //     msg_timestamp = millis();
+    //     sendTestMessage();
+    // }
+
     if(!client.connected())
     {
         reconnect();
     }
     client.loop();
 }
+
+// static void sendTestMessage(void)
+// {
+//     String topic = "test/";
+//     topic += getDeviceName().c_str();
+//     topic += "/temperature";
+//     String payload = "25.6";
+
+//     client.publish(topic.c_str(), payload.c_str());
+//     Serial.println("MQTT Publish topic: " + topic);
+//     Serial.println("MQTT Publish payload: " + payload);
+// }
