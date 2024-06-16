29de9f0dfad367533e03cbdbe5b18f8b7131b5e3
szymonpolitowski <szymon.politowski.96@gmail.com>
2024-06-16 09:29:16 +0200

    main: add sensor manager

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
 #include "mem_flash.hpp"
 #include "wifi_sta.hpp"
 #include "app_mqtt.hpp"
+#include "app_sensor.hpp"
 
 void setup()
 {
   delay(100);
   Serial.begin(115200);
   while(!Serial);
-
+  Serial.println("");
   Serial.println("Hello, ESP8266!");
 
   initMemFlash();
@@ -18,10 +19,13 @@ void setup()
 
   wifiConnect();
   mqttInit();
+  sensorInit();
+  sensorSetInterval(getDeviceInterval());
 }
 
 void loop()
 {
+  sensorLoop();
   mqttLoop();
   delay(10);
 }
